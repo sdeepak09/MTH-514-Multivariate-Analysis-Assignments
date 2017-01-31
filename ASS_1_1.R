1ed79422af8484a96e617563bbbf2d2ca4793ecf
sdeepak09 <sdeepak@iitk.ac.in>
2017-02-01 00:44:31 +0530

    ## ASS_2_1 completed

--- ASS_1_1.R
+++ ASS_1_1.R
@@ -10,7 +10,7 @@ eig_val=eig_val_vec$values
 eig_vec=eig_val_vec$vectors
 data_new=as.matrix(data_cen_mat)%*%eig_vec[,1:3]
 library(rgl)
-plot3d(data_new,type = 's',xlab = "PCA1",ylab = "PCA2",zlab =  "PCA3",radius = 0.01)
+plot3d(data_new,type = 's',xlab = "PC1",ylab = "PC2",zlab =  "PC3",radius = 0.01)
 text3d(data_new,texts=rownames(data),col="red")
 
 
@@ -25,12 +25,12 @@ eig_val_1=eig_val_vec_1$values
 eig_vec_1=eig_val_vec_1$vectors
 data_new_1=as.matrix(data_cen_mat_1)%*%eig_vec_1[,1:3]
 View(data_new_1)
-plot3d(data_new_1,type = 's',xlab = "PCA1",ylab = "PCA2",zlab =  "PCA3",radius = 0.01)
+plot3d(data_new_1,type = 's',xlab = "PC1",ylab = "PC2",zlab =  "PC3",radius = 0.01)
 text3d(data_new_1,texts=as.factor(c(1:103)),col="red")
 
 
 ## Clustering of the countries
-clust=kmeans(data_new_1,4, iter.max = 100,nstart = 3)
+clust=kmeans(data_new_1,5, iter.max = 100,nstart = 3)
 cl=clust$cluster
 plot3d(data_new_1,type = 's',col=cl,xlab = "PCA1",ylab = "PCA2",zlab =  "PCA3",radius = 0.3)
 
@@ -40,8 +40,8 @@ country=as.vector(comptd_data[,1])
 country_wo_outlr=country[-c(3,5,8,10,14,15,25,42,57,60,63,64,72,93,98,106,113)]
 PCA1=data_new_1[,1]
 dt=data.frame(country_wo_outlr,PCA1)
-dt1=dt[order(dt[,2]),]
-View(dt1)   ## Not Good 
+dt1=dt[order(-dt[,2]),]
+View(dt1)   ## Good 
 
 ## Variable Clustering
 first_3_PCs=eig_vec_1[,1:3]
